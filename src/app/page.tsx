39e230802e2247d8c07c9fc321cd4a46313ab019
Christian Javier Cedillo Palacios <christian.cedillo.palacios@outlook.com>
2024-07-10 15:10:11 -0600

    header with navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
+"use client";
 import { Button } from "@/components/ui/button";
+import { Title } from "@/components/shared/Title";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
-      <p className="text-2xl text-primary font-semibold">
-        Rick and Morty with Next js and Graphql
-      </p>
+      <Title title="Rick and Morty App" />
+
       <Button>Iniciar</Button>
     </main>
   );
