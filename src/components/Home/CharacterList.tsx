c932e3152d592bad2bba63e0795b8a3c6beac84e
Christian Javier Cedillo Palacios <christian.cedillo.palacios@outlook.com>
2024-07-10 20:15:52 -0600

    character detail page

--- src/components/Home/CharacterList.tsx
+++ src/components/Home/CharacterList.tsx
@@ -1,14 +1,15 @@
 "use client";
 import { useEffect, useState } from "react";
-import { Button } from "../ui/button";
 import { CharacterCard } from "../shared/CharacterCard";
 import { useSuspenseQuery } from "@apollo/client";
 import { type CharactersData } from "@/types/types";
 import { GET_CHARACTERS_BY_PAGE_AND_NAME } from "@/querys/queryCharacters";
 import { Pagination } from "./Pagination";
+import { useRouter } from "next/navigation";
 import { Input } from "../ui/input";
 
 export function CharacterList() {
+  const router = useRouter();
   const [currentPage, setCurrentPage] = useState(1);
   const [searchName, setSearchName] = useState("");
 
@@ -50,7 +51,11 @@ export function CharacterList() {
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 pt-10">
         {data?.characters.results.length > 0 ? (
           data?.characters.results.map((character) => (
-            <CharacterCard key={character.id} character={character} />
+            <CharacterCard
+              key={character.id}
+              character={character}
+              action={() => router.push(`/${character.id}`)}
+            />
           ))
         ) : (
           <p className="font-semibold">Sin resultados</p>
